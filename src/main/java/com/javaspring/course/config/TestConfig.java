9f2df72f9cd5ade66175c66b99611770056ebc0a
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2024-12-09 12:33:54 -0300

    OrderStatus enum

--- src/main/java/com/javaspring/course/config/TestConfig.java
+++ src/main/java/com/javaspring/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.javaspring.course.entities.Order;
 import com.javaspring.course.entities.User;
+import com.javaspring.course.enums.OrderStatus;
 import com.javaspring.course.repositories.OrderRepository;
 import com.javaspring.course.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
 		User user2 = new User(null, "User Test Number 2", "user2@gmail.com", "977777777", "123456");
 		userRepository.saveAll(Arrays.asList(user1,user2));
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID ,user1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,user2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED,user1);
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		
 	}
--- src/main/java/com/javaspring/course/entities/Order.java
+++ src/main/java/com/javaspring/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.javaspring.course.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -24,16 +25,20 @@ public class Order {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
 	
+	
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -51,6 +56,14 @@ public class Order {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		this.orderStatus = orderStatus.getCode();
 	}
 
 	public User getClient() {
