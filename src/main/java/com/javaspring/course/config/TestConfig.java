ffd28d1a9f11e3f6fcc39324dcb1dc5c635a534c
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2024-12-11 00:14:59 -0300

    Category

--- src/main/java/com/javaspring/course/config/TestConfig.java
+++ src/main/java/com/javaspring/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.javaspring.course.entities.Category;
 import com.javaspring.course.entities.Order;
 import com.javaspring.course.entities.User;
 import com.javaspring.course.enums.OrderStatus;
+import com.javaspring.course.repositories.CategoryRepository;
 import com.javaspring.course.repositories.OrderRepository;
 import com.javaspring.course.repositories.UserRepository;
 
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -35,5 +40,9 @@ public class TestConfig implements CommandLineRunner {
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED,user1);
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 	}
 }
