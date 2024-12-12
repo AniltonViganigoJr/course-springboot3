a9ec41a3d6002b0f0a95ce043dd776e4aa47022b
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2024-12-11 23:42:03 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/javaspring/course/entities/Order.java
+++ src/main/java/com/javaspring/course/entities/Order.java
@@ -1,7 +1,9 @@
 package com.javaspring.course.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.javaspring.course.enums.OrderStatus;
@@ -12,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -31,6 +34,8 @@ public class Order {
 	@JoinColumn(name = "client_id")
 	private User client;
 	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Order() {
 	}
@@ -74,6 +79,9 @@ public class Order {
 		this.client = client;
 	}
 
+	public Set<OrderItem> getItems(){
+		return items;
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
